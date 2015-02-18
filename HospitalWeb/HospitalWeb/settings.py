c2f1a1f8e422d1ead39e917af3d4105f22f17ac8
HugoCapitan <hmoravilez@gmail.com>
2015-02-17 21:18:31 -0600

    Correxion de errores por cambio de db

--- HospitalWeb/HospitalWeb/settings.py
+++ HospitalWeb/HospitalWeb/settings.py
@@ -57,13 +57,13 @@ WSGI_APPLICATION = 'HospitalWeb.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mydb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'myuser',
-        'PASSWORD': 'password',
+        'PASSWORD': 'n0m3l0',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
