59b386c8d839577722e6eba5dc320aaffc5f6c30
HugoCapitan <hmoravilez@gmail.com>
2015-02-26 16:56:37 -0600

    Made requirements.txt

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,7 +3,7 @@ from .views import AppointmentDetailView_Patient, AppointmentKindListView, Appoi
 
 urlpatterns = patterns('',
 	url(r'^citas/tipos/$', AppointmentKindListView.as_view(), name='appointmentkindlistview'),
-    url(r'^citas/tipos/(?P<kind>[\w\-]+)/$', AppointmentKindListView.as_view(), name='appointmentkindlistview'),
+    url(r'^citas/tipos/?(?P<kind>[\w\-]+)/$', AppointmentKindListView.as_view(), name='appointmentkindlistview'),
 	url(r'^citas/(?P<slug>[\w\-\W]+)/$', AppointmentKindDetailView.as_view(), name='appointmentkindlistview'),
     url(r'^pacientes/cita/(?P<slug>[\w\W]+)/$', AppointmentDetailView_Patient.as_view(), name='appointment_detail'),
     url(r'^pacientes/nueva/$', AppointmentAddPatientView.as_view(), name="appointment_new")
--- publicActions/tests.py
+++ publicActions/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.models import User
+from .models import Announcement
+from django.utils.text import slugify
 
-# Create your tests here.
+class WorkTets(TestCase):
+
+	def setUp(self):
+		self.test_user = User.objects.create_user(username= 'testing', password = '123')
+
+	def test_add_adress(self):
+			title = "Alfredo MVP"
+			content = "Contenido Contenido Contenido Contenido Contenido Contenido Contenido Contenido Contenido Contenido Contenido " 
+			date = '12/12/12'
+			image = SimpleUploadedFile(name='test_image.jpg', content=open(image_path, 'rb').read(), content_type='image/jpeg')
