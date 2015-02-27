c663abd9c93a8a8a9b203d72f7a9203668571bb8
Hugo Fernando Mora Avilez <hmoravilez@gmail.com>
2015-02-27 08:41:40 -0600

    Update tests.py

--- publicActions/tests.py
+++ publicActions/tests.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+from .models import Announcement
+
+class LogicTest(TestCase):
+
+	# tests de logica
+	def setUp(self):
+		# se da de alta un usuario y un paciente bien
+		test_ann_user = User.objects.create(username='usertest', first_name='Tests', last_name='User', email='user@test.com')
+		test_ann = Announcement.objects.create(title='Testing', content='sajfskbfaksdf38233sd fsd fg sdf sd fs', image='eSfafem/asfdsa')
