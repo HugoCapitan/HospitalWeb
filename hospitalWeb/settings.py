1f2a74c19acf5c54fc4d41d7c0651a2d67a2846e
HugoCapitan <hmoravilez@gmail.com>
2015-02-27 00:16:45 -0600

    Trabajando en los tests unitarios

--- hospitalWeb/settings.py
+++ hospitalWeb/settings.py
@@ -74,26 +74,26 @@ ROOT_URLCONF = 'hospitalWeb.urls'
 WSGI_APPLICATION = 'hospitalWeb.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'mydb',                      # Or path to database file if using sqlite3.
-		# The following settings are not used with sqlite3:
-		'USER': 'myuser',
-		'PASSWORD': 'n0m3l0',
-		'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-		'PORT': '',                      # Set to empty string for default.
-	}
-}
-
+# Database with Postgres
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+# 		'NAME': 'mydb',                      # Or path to database file if using sqlite3.
+# 		# The following settings are not used with sqlite3:
+# 		'USER': 'myuser',
+# 		'PASSWORD': 'n0m3l0',
+# 		'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+# 		'PORT': '',                      # Set to empty string for default.
+# 	}
 # }
+
+# Database with SQLite3
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Internationalization
--- patients/tests.py
+++ patients/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Patient
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LogicTest(TestCase):
+
+	# tests de logica
+	def setUp(self):
+		# se da de alta un usuario y un paciente bien
+		test_pat_user = User.objects.create(username='usertest', first_name='Tests', last_name='User', email='user@test.com')
+		test_pat = Patient.objects.create(user=test_pat_user, curp='sajfskbfaksdf38233', birth='1999-12-12', phone='+525523870166')
+
+	# def test_existe_(self):
+	# 	res = test_pat_user.
+	# 	res = self.client.get('/artists/%d/' % self.artist.id)
+	# 	self.assertEqual(res.status_code, 200)
+	# 	self.assertTrue('AC/DC' in res.content)
+
+	# def test_usuario_logeado(self):
+	# 	res = self.client.get('/tracks/%s/' % self.track.title)
+	# 	self.assertEqual(res.status_code, 302)
+
+	# def test_no_existe_vista(self):
+	# 	id_viejo = self.artist.id
+	# 	self.artist.delete()
+	# 	res = self.client.get('/artists/%d/' % id_viejo)
+	# 	self.assertEqual(res.status_code, 404)
